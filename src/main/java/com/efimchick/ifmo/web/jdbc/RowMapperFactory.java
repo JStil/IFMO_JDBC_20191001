be0645caeaaa7233bf20ea64e29eb95cb5de4605
JStil <dzhey.stil@bk.ru>
2020-03-25 20:48:09 +0300

    implemented rowmapper

--- src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -1,10 +1,31 @@
 package com.efimchick.ifmo.web.jdbc;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+
+import java.math.BigInteger;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class RowMapperFactory {
 
     public RowMapper<Employee> employeeRowMapper() {
-        throw new UnsupportedOperationException();
+        return RS -> {
+            try {
+                return new Employee(
+                    new BigInteger(RS.getString("id")),
+                    new FullName(RS.getString("firstname"), RS.getString("lastname"), RS.getString("middlename")),
+                    Position.valueOf(RS.getString("position")),
+                    LocalDate.parse(RS.getString("hiredate")),
+                    RS.getBigDecimal("salary")
+                );
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        };
     }
 }
+
+
